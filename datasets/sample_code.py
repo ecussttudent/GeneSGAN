04de3b5caa7f7d85c3a20a006d05a104ad76b705
ChenZhiJun <24013283@mail.ecust.edu.cn>
2025-10-23 22:08:22 +0800

    Update  training parameters

--- datasets/sample_code.py
+++ datasets/sample_code.py
@@ -10,10 +10,10 @@ if __name__ == '__main__':
 	image_data = pd.read_csv('toy_data_imaging.csv')
 	gene_data = pd.read_csv('toy_data_gene.csv')
 
-	fold_number = 1
+	fold_number = 2
 	ncluster = 3
-	start_saving_epoch = 20000
-	max_epoch = 30000
+	start_saving_epoch = 2000
+	max_epoch = 3000
 	WD = 0.11
 	AQ = 30
 	cluster_loss = 0.01
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 
 
 	cross_validated_clustering(image_data, gene_data, ncluster, fold_number, 0.8, start_saving_epoch, max_epoch, output_dir, WD, AQ, cluster_loss,\
-		genelr = 0.0002, batchSize=120, lipschitz_k=0.5)
+		genelr = 0.0002, batchSize=25, lipschitz_k=0.5)
 
 
 
