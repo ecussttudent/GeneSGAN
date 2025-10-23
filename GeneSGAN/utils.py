9f1be6cbe105c3cf024ec52ae758a22c27fbb236
ChenZhiJun <24013283@mail.ecust.edu.cn>
2025-10-23 21:09:53 +0800

    debug

--- GeneSGAN/utils.py
+++ GeneSGAN/utils.py
@@ -150,7 +150,12 @@ def parse_train_data(imaging_data, gene_data, covariate, random_seed, data_fract
 	indices_cn = np.random.choice(n_cn_data, int(data_fraction*n_cn_data), replace=False)
 	cn_data = imaging_data.loc[imaging_data['diagnosis'] == -1].drop(['participant_id','diagnosis'], axis=1).values[indices_cn]
 	pt_data = imaging_data.loc[imaging_data['diagnosis'] !=- 1].drop(['participant_id','diagnosis'], axis=1).values[indices_pt]
-	gene_data = imaging_data[['participant_id']].merge(gene_data,on='participant_id')
+	
+	#gene_data = imaging_data[['participant_id']].merge(gene_data,on='participant_id')
+	gene_data_fillna = gene_data.copy()
+	numeric_cols = gene_data.select_dtypes(include=['number']).columns
+	gene_data_fillna[numeric_cols] = gene_data[numeric_cols].fillna(gene_data[numeric_cols].mean())
+	
 	gene_data_fillna = gene_data.fillna(gene_data.mean())
 	gene_data = gene_data.drop(['participant_id'], axis=1).values[indices_pt]
 	gene_data_fillna = gene_data_fillna.drop(['participant_id'], axis=1).values[indices_pt]
